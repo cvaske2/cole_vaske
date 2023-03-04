12d3507b6f86bf2e8ca9753edf2ddbc85ab282ac
Cole Patrick Vaske <cvaske@csce.cs.unl.edu>
2023-03-03 22:32:33 -0600

    Fixed a little architecture; added styling; added new warning box; quiz5 for csce464

--- include/callbacks.php
+++ include/callbacks.php
@@ -71,4 +71,50 @@
             </div>
         ";
     }
+
+    function displayClassBlock($BASE_DIR = "../")
+    {
+        return "
+            <style type='text/css'>
+                .container-warning {
+                    display: grid;
+                    grid-template-rows: 1fr;
+                    grid-template-columns: .1fr 1fr .1fr;
+                    border: 3px solid red;
+                    background: black;
+                    margin: 2vw;
+                }
+                .container-class-warning {
+                    display: grid;
+                    grid-template-rows: 1fr;
+                    grid-template-columns: .1fr 1fr .1fr;
+                    border: 3px solid yellow;
+                    background: black;
+                    margin: 2vw;
+                }
+                .warning {
+                    font-size: 20px;
+                }
+                .warning-png {
+                    display: block;
+                    margin: auto;
+                    width: 70px;
+                    height: auto;
+                }
+                .warn-text {
+                    color: #ffffff;
+                    font-family: monospace;
+                    font-size: 18px;
+                    padding-left: 15px;
+                    padding-right: 15px;
+                  }
+            </style>
+            <div class='container-class-warning'>
+                <!-- Image obtained from https://openclipart.org/ -->
+                <img class='warning-png' src='".$BASE_DIR."images/yellow_warning.png'>
+                <p class='warn-text'>This page is being used for a class-related project. Its contents may not make sense with the theme of the page for the purpose of meeting certain requirements, and its state is not final.</p>
+                <img class='warning-png' src='".$BASE_DIR."images/yellow_warning.png'>
+            </div>
+        ";
+    }
 ?>
--- pages/contact.php
+++ pages/contact.php
@@ -8,6 +8,7 @@
             <head>
                 <title>Cole Vaske</title>
                 <link rel='stylesheet' href='".$BASE_DIR_PREFIX."include/styles.css'>
+				<link rel='stylesheet' href='".$BASE_DIR_PREFIX."include/contact.css'>
             </head>
             <a id='top'></a>
             <header>
@@ -30,12 +31,84 @@
                 </ul>
             </header>
             ".displayWarningBlock()."
+			".displayClassBlock()."
             <main>
+				<form id='contactForm'>
+					<fieldset>
+						<legend>Message</legend> 
+						<table>
+							<tr>
+							<td>Type your Message here</td>
+							</tr>
+							<tr>
+								<td><textarea name='message' id='message' rows='5' cols='70'></textarea></td>
+							</tr>
+							<tr>
+								<td>Rate my site out of 5 stars!</td>
+							</tr>
+						</table>
+						<table>
+							<tr>
+								<td><i>[1 star]</i></td>
+								<td><input type='radio' id='rad1' name='radio-group'></td>
+								<td><input type='radio' id='rad2' name='radio-group'></td>
+								<td><input type='radio' id='rad3' name='radio-group'></td>
+								<td><input type='radio' id='rad4' name='radio-group'></td>
+								<td><input type='radio' id='rad5' name='radio-group'></td>
+								<td><i>[5 star]</i></td>
+							</tr>
+						</table>
+						<br>
+						<table>
+							<tr>
+								<td>Leave your e-mail so I can contact you</td>
+							</tr>
+							<tr>
+								<td>
+									<input class='email-txt' type='text' name='email' id='email'>
+								</td>
+							</tr>
+						</table>
+					</fieldset>
+				<td>
+					<button class='submit-btn' onclick='submitForm()'>Submit Button</btn>
+				</td>
+			</form>
             </main>
             <footer>
                 <a href='#top'>Back to top</a>
             </footer>
         </html>
+		<script>
+			function getRating(form) {
+				if (form.rad1.checked)
+				return 1;
+				else if (form.rad2.checked)
+				return 2;
+				else if (form.rad3.checked)
+				return 3;
+				else if (form.rad4.checked)
+				return 4;
+				else if (form.rad5.checked)
+				return 5;
+				return null;
+			}
+			console.log('we in here');
+			function submitForm() {
+				var contactForm = document.getElementById('contactForm');
+				console.log(contactForm);
+				var message = contactForm.message.value;
+				var rating = getRating(contactForm);
+				var email = contactForm.email.value;
+
+				rating = rating === null ? '<no rating given>' : rating + '/5';
+				
+				var word_count = message === '' ? 0 : (message.match(/ /g) || []).length + 1;
+
+				var output = `Number of words: \${word_count}\nYour rating: \${rating}\nYour supplied e-mail: \${email}`;
+				alert(output);
+			}
+		</script>
     ";
 
     echo $html_string;
