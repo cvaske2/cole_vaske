1fbcbe65614b3ecb5f75c02618d6d0f81b4d24eb
Cole Patrick Vaske <cvaske@csce.cs.unl.edu>
2023-03-19 23:50:46 -0500

    EWB stub

--- pages/ewb.php
+++ pages/ewb.php
@@ -32,6 +32,10 @@
             </header>
             ".displayClassBlock()."
             <main>
+                <h1>University of Nebraska Engineers Without Borders</h1>
+                <p>I have been a member of the University of Nebraska student chapter of Engineers Without Borders (EWB-NU) for my entire collegiate career. The chapter is responsible for a bridge project in Zambia and a solar panel project in Madagascar, as well as a number of local outreach events and programs.</p>
+                <p>During my time at EWB, I have been treasurer, fundraising lead and manager of the \"Nebraska Team\", and vice president.</p>
+                <p>This page and its subpages details EWB-NUs and its projects, events, and my involvement in the organization.</p>
             </main>
             <footer>
                 <a href='#top'>Back to top</a>
